1edbf141e5defeb9d877fa02d2497b208fddbe6a
NazhRomar <81796887+NazhRomar@users.noreply.github.com>
2025-01-21 08:37:31 +0800

    Create sample1.php

---
+++ ICS2609 APPLICATIONS DEVELOPMENT AND EMERGING TECHNOLOGIES 2 (ENTERPRISE- BACK-END)/sample1.php
@@ -0,0 +1,39 @@
+RONAN MIGUEL BARNACHEA, Now
+<?php 
+
+$message = "Hello World";
+$array  = [353, 344, 554];
+
+
+// echo $message;
+// var_dump($message); // checks the datatype
+echo $message;
+var_dump($array);
+print "hallo <br>";
+
+echo "Message = ".$message."<br>";
+
+$first = "Nash";
+$last = "Aguas";
+$mid = "J";
+$age = 45;
+
+echo $last.", ".$first.", ".$mid.".<br>";
+echo $age;
+
+// die("Hello"); // used for try and catch / error (it will not display the next line)
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world! <?php echo $first." bembang aguas";?></h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
