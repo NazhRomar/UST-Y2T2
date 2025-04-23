e3f507205932b3827457fa4d637d6d61296c0f8c
Nazh <nazhromar@gmail.com>
2025-04-23 15:42:08 +0800

    Update insert.php

--- ICS2609/04-23/insert.php
+++ ICS2609/04-23/insert.php
@@ -142,13 +142,14 @@
 </html>
 
 <?php
+require_once "dbaseconnection.php";
 // database connection
-$nraservername = "localhost"; // offline default server name
-$nrausername = "root"; // username from MySQL
-$nrapassword = "";
-$nradbase = "db_hotel";
+// $nraservername = "localhost"; // offline default server name
+// $nrausername = "root"; // username from MySQL
+// $nrapassword = "";
+// $nradbase = "db_hotel";
 
-$nraconn = new mysqli($nraservername, $nrausername, $nrapassword, $nradbase);
+// $nraconn = new mysqli($nraservername, $nrausername, $nrapassword, $nradbase);
 
 // Check connection
 if (!$nraconn->connect_error) {
