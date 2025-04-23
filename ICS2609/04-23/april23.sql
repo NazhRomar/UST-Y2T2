e2c79e24b0c9008613105e25b8cd7180db8af44c
Nazh <nazhromar@gmail.com>
2025-04-23 15:44:59 +0800

    Add files via upload

---
+++ ICS2609/04-23/april23.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: db_hotel
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbl_accountdetails`
+--
+
+DROP TABLE IF EXISTS `tbl_accountdetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_accountdetails` (
+  `account_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(60) DEFAULT NULL,
+  `lname` varchar(45) DEFAULT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone_num` bigint(20) DEFAULT NULL,
+  `addinfo` varchar(100) DEFAULT NULL,
+  `account_type` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `img_path` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='		';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_accountdetails`
+--
+
+LOCK TABLES `tbl_accountdetails` WRITE;
+/*!40000 ALTER TABLE `tbl_accountdetails` DISABLE KEYS */;
+INSERT INTO `tbl_accountdetails` VALUES (35,'Nazh','Arcedo','Male','234234','nazhromar@gmail.com',13123,'aweaeaawe','Admin','waeawe','61f87e32e60969895c61859f7be3ca50','nra_images/Screenshot 2025-03-26 092203.png');
+/*!40000 ALTER TABLE `tbl_accountdetails` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-23 15:43:36
